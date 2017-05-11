8992889d6cbbaaffcef2c68d1d3c88188da35c43
Daniel Silva <danny@dannyps.net>
2017-05-11 14:11:12 +0100

    Implement getEnterOrString()

--- progT2/Misc.h
+++ progT2/Misc.h
@@ -10,3 +10,21 @@ void printTime(unsigned t) {
 	cout << setfill('0') << setw(2) << t / 60 << ":";
 	cout << setfill('0') << setw(2) << t % 60;
 }
+
+pair<bool, string> getEnterOrString() {
+	
+	pair<bool, string> res;
+	char c;
+	c = getchar();
+	if (c == '\n') {
+		res.first = true;
+		return res;
+	}
+	else {
+		string aux;
+		getline(cin, aux);
+		res.first = false;
+		res.second = c + aux;
+		return res;
+	}
+}
